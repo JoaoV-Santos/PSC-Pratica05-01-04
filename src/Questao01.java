996b77d265e8a8c01d6765d791fc983e0599ad06
João Victor Santos <68966095+JoaoV-Santos@users.noreply.github.com>
2024-04-01 19:58:02 -0300

    questao 02

--- src/Questao01.java
+++ src/Questao01.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class Questao01 {
     public static void main(String[] args) {
-
+        
     }
 }
--- src/Questao02.java
+++ src/Questao02.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Questao02 {
 	public static void main(String[] args) {
 		
