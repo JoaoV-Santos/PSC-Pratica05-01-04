e251516e844a87224bd6d3057d6059000d1c66c6
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-02 10:37:29 -0300

    resoluÃcao da questao 06

--- src/Questao02.java
+++ src/Questao02.java
@@ -1,7 +1,12 @@
-import java.util.Scanner;
 
 public class Questao02 {
 	public static void main(String[] args) {
-		
+		int i = 1, j = 60;
+
+		while (j >= 0) {
+			System.out.print("I=" + i + " J=" + j + "\n");
+			i += 3;
+			j = j - 5;
+		}
 	}
 }
