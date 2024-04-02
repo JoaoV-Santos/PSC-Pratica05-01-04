0dfee5e4021c5cce28024b78122ee74d368df2fb
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-02 10:18:04 -0300

    resoluçcao da questao 02

--- src/Questao01.java
+++ src/Questao01.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class Questao01 {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        double x1 = in.nextDouble();
+        double y1 = in.nextDouble();
+
+        double x2 = in.nextDouble();
+        double y2 = in.nextDouble();
+
+        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.printf("%.4f\n" , d);
+
+
     }
 }
